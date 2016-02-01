5b6eb01a8ce19d582d1de47f99f01a84d5ea1f6d
Maarten A. Breddels <maartenbreddels@gmail.com>
2016-02-01 14:02:48 +0100

    added swapfint8

--- sub/swapfint.c
+++ sub/swapfint.c
@@ -16,12 +16,29 @@ Use:        fint swapfint(fint arg)
 
 Updates:    Mar  9, 1994: JPT, Document created
 #<
+#> swapfint8.dc3
+Function:   swapfint8
 
+Purpose:    Swap the bytes of a fint8
+
+Category:   SYSTEM
+
+File:       swapfint8.c
+
+Author:     J.P. Terlouw
+
+Use:        fint8 swapfint8(fint8 arg)
+ 
+            swapfint8   returns the arg, with its bytes reversed in order.
+
+Updates:    Mar  9, 1994: JPT, Document created
+#<
 #> swapfint.h
 #if !defined(_swapfint_h_)
 #define _swapfint_h_
 #include "gipsyc.h"
 extern fint swapfint(fint arg);
+extern fint8 swapfint8(fint8 arg);
 #endif
 #<
 */
@@ -40,6 +57,22 @@ extern fint swapfint(fint arg)
 
    i=0;
    j=sizeof(fint);
+   do out.b[--j] = in.b[i++]; while(j);
+   return out.l;
+}
+extern fint8 swapfint8(fint8 arg)
+{
+   int i,j;
+   
+   union {
+      fint8 l;
+      char b[sizeof(fint8)];
+   } in, out;
+    
+   in.l = arg;
+
+   i=0;
+   j=sizeof(fint8);
    do out.b[--j] = in.b[i++]; while(j);
    return out.l;
 }
