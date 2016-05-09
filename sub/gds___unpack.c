8233420d225e1f2acb541fd5cf0a62f7a3f13783
maartenbreddels <maartenbreddels@gmail.com>
2016-05-09 12:07:39 +0200

    removed anyout, caused linker error

--- sub/gds___unpack.c
+++ sub/gds___unpack.c
@@ -50,8 +50,8 @@ Use:           INTEGER*8 GDS___UNPACK( SET,             Input        character
 
                COORD_WORD     coordinate word from which grid is unpacked
 
-               IAX            axis number of grid value 
-                              ( 0 ... N-1 with N axes ) 
+               IAX            axis number of grid value
+                              ( 0 ... N-1 with N axes )
 
                ERR            error return code :
                               =  0 : successful
@@ -64,12 +64,12 @@ Updates:       Dec  5, 1989: WZ,  migrated to C
                Apr 09, 2009: VOG, Changed definition of NINT to force correct
                                   processing of coordinates when CRPIX ends
                                   on .5 in all functions dealing with coordinates.
-               
+
 #<
 
 @ integer*8 function gds___unpack( character,
 @                                integer*8,
-@                                integer, 
+@                                integer,
 @                                integer )
 
 ----------------------------------------------------------------------------*/
@@ -97,13 +97,13 @@ fint8   gds___unpack_c( fchar     set,               /* name of set          */
       factor = setinfo->factor[*iax+1];
       f_coord = ( *coord_word % factor ) / setinfo->factor[*iax];
    }
-   anyoutf(1, "unpack: %lld %lld %lld %d\n", *coord_word, f_coord, factor, *iax);
+//   anyoutf(1, "unpack: %lld %lld %lld %d\n", *coord_word, f_coord, factor, *iax);
    if ( f_coord == 0 ) {
       f_coord = 1;
       *err = GDS_COORDUNDEF;
    }
    origin = setinfo->origin[*iax];
-   return( f_coord - NINT( origin ) ); 
+   return( f_coord - NINT( origin ) );
 }
 
 /*============================================================================
@@ -137,21 +137,21 @@ Use:           CALL GDS___PACK( SET,               Input     character
                IAX          axis number of grid value
                             ( 0 ... N-1 with N axes )
 
-               ERR          =  0 : successful  
+               ERR          =  0 : successful
                              -34 : grid < minimum or grid > maximum (limited)
                              -35 : grid > maximum of last axis      (limited)
                                    In this case the axis can be extended.
-                           
+
                Pack a grid coordinate only after a call gds___unpack with
                err=-19 to check whether this axis is undefined.
 
 Updates:       Dec 5,  1989: WZ, migrated to C
                Oct 31, 1990: WZ, 'naxis', etc. from setsta -> dsc_file
                Mar 23, 1994: JPT, modified to cooperate with GDS server
-               
+
 #<
 
-@ subroutine gds___pack( character, 
+@ subroutine gds___pack( character,
 @                        integer*8,
 @                        integer*8,
 @                        integer,
@@ -169,7 +169,7 @@ void  gds___pack_c( fchar     set,          /* name of set                  */
    fint     naxis, size, err_i;
    fint8    grid, min_g, max_g, factor;
    int      extend, fix;
-   
+
    err_i = gds_rhed(set,&setinfo);
    if (gds___fail(!err_i,err_i,err)) return;
    naxis = setinfo->naxis;
@@ -188,8 +188,8 @@ void  gds___pack_c( fchar     set,          /* name of set                  */
       grid = *coord;
    factor = setinfo->factor[*iax];
    *coord_word += ( grid - min_g + 1 ) * factor;
-   anyoutf(1,"pack: %lld %lld %lld %lld %d %d\n", *coord_word, *coord, min_g, max_g, *iax, naxis);
-   if (gds___fail( !fix, GDS_COUTRANGE, err )) return; 
+   //anyoutf(1,"pack: %lld %lld %lld %lld %d %d\n", *coord_word, *coord, min_g, max_g, *iax, naxis);
+   if (gds___fail( !fix, GDS_COUTRANGE, err )) return;
                                         /* coordinate outside axis limits ? */
    if (gds___fail( !extend, GDS_CTOOBIG, err )) return;
                                                   /* coordinate > maximum ? */
